0f88916eae903cb4d01f003032db41116f7214b8
SergiiOshmarin <113200315+SergiiOshmarin@users.noreply.github.com>
2022-12-26 18:33:28 +0100

    working

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-// Send the request to the server when the form is submitted
-document.getElementById('formChat').addEventListener('submit', async (event) => {
-  event.preventDefault();
-  // Send the request to the server
-  const response = await fetch(`http://localhost:8000`);
-  // Get the response data
-  const data = await response.text();
-  // Display the response data in the page
-  document.getElementById('subscribe').innerHTML = data;
-});
+document.getElementById("formChat").addEventListener("submit", function(event) {
+    // Prevent the form from being submitted
+    event.preventDefault();
+  
+    // Get the number of days
+    const days = document.getElementById("textField").value;
+  
+    // Create a WebSocket connection to the server
+    const ws = new WebSocket("ws://localhost:8000");
+  
+    ws.onopen = function() {
+      // Send the request to the server
+      ws.send(days);
+    };
+  
+    // Display the response from the server
+    ws.onmessage = function(event) {
+        const currentContent = document.getElementById("subscribe").innerHTML;
+        document.getElementById("subscribe").innerHTML = currentContent + "<br>"  + "Privat API Answer" + event.data;
+      };
+  });
+  
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import platform
-import aiohttp
 import asyncio
+import logging
+import websockets
+import aiohttp
 from datetime import datetime, timedelta
+from websockets import WebSocketServerProtocol
+from websockets.exceptions import ConnectionClosedOK
+
+logging.basicConfig(level=logging.INFO)
 
 
-async def main(days: int):
+async def api(days: int):
     results = []
     async with aiohttp.ClientSession() as session:
         for i in range(days):
             # Calculate the date for the request
-            date = (datetime.now() - timedelta(days=i) -
-                    timedelta(days=1)).strftime('%d.%m.%Y')
+            date = (datetime.now() - timedelta(days=i)).strftime('%d.%m.%Y')
             # Make the request
             async with session.get(f'https://api.privatbank.ua/p24api/exchange_rates?json&date={date}') as response:
                 result = await response.json()
@@ -26,40 +30,54 @@ async def main(days: int):
     return results
 
 
-async def handle_client(reader, writer):
-    # Read the data from the client
-    data = await reader.read()
-    # Decode the data as a string
-    days_str = data.decode()
-    # Check if the input is a valid number of days
-    try:
-        days = int(days_str)
-        if days < 1 or days > 10:
-            raise ValueError
-    except ValueError:
-        # Send an error message to the client if the input is not a valid number of days
-        writer.write(
-            "Error: Please enter a number of days between 1 and 10".encode())
-        await writer.drain()
-    else:
-        # Get the exchange rates
-        rates = await main(days)
-        # Encode the rates as bytes and write them to the client
-        writer.write(str(rates).encode())
-        await writer.drain()
-    finally:
-        # Close the connection
-        writer.close()
+class Server:
+    clients = set()
+
+    async def register(self, ws: WebSocketServerProtocol):
+        self.clients.add(ws)
+        logging.info(f'{ws.remote_address} connects')
+
+    async def unregister(self, ws: WebSocketServerProtocol):
+        self.clients.remove(ws)
+        logging.info(f'{ws.remote_address} disconnects')
+
+    async def send_to_clients(self, message: str):
+        if self.clients:
+            [await client.send(message) for client in self.clients]
+
+    async def ws_handler(self, ws: WebSocketServerProtocol):
+        await self.register(ws)
+        try:
+            await self.distribute(ws)
+        except ConnectionClosedOK:
+            pass
+        finally:
+            await self.unregister(ws)
+
+    async def distribute(self, ws: WebSocketServerProtocol):
+        async for message in ws:
+            # Check if the input is a valid number of days
+            try:
+                days = int(message)
+                if days < 1 or days > 10:
+                    raise ValueError
+            except ValueError:
+                # Send an error message to the client if the input is not a valid number of days
+                await ws.send("Error: Please enter a number of days between 1 and 10")
+            except Exception:
+                # Send an error message to the client if the input is not a valid number
+                await ws.send("Error: Please enter a valid number")
+            else:
+                # Get the exchange rates
+                rates = await api(days)
+                # Encode the rates as bytes and write them to the client
+                await ws.send(str(rates))
 
 
-async def start_server():
-    if platform.system() == 'Windows':
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+async def main():
+    server = Server()
+    async with websockets.serve(server.ws_handler, 'localhost', 8000):
+        await asyncio.Future()  # run forever
 
-    # Create the server
-    server = await asyncio.start_server(handle_client, 'localhost', 8000)
-    # Run the server until it is cancelled
-    await server.serve_forever()
-
-if __name__ == "__main__":
-    asyncio.run(start_server())
+if __name__ == '__main__':
+    asyncio.run(main())
