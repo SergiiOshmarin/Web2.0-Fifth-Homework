f3045722d585ce1377ae0c80641e939fbdf0813c
SergiiOshmarin <113200315+SergiiOshmarin@users.noreply.github.com>
2022-12-26 18:53:04 +0100

    final

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 document.getElementById("formChat").addEventListener("submit", function(event) {
-    // Prevent the form from being submitted
-    event.preventDefault();
-  
-    // Get the number of days
-    const days = document.getElementById("textField").value;
-  
-    // Create a WebSocket connection to the server
-    const ws = new WebSocket("ws://localhost:8000");
-  
-    ws.onopen = function() {
-      // Send the request to the server
-      ws.send(days);
-    };
-  
-    // Display the response from the server
-    ws.onmessage = function(event) {
-        const currentContent = document.getElementById("subscribe").innerHTML;
-        document.getElementById("subscribe").innerHTML = currentContent + "<br>"  + "Privat API Answer" + event.data;
-      };
-  });
-  
+  // Prevent the form from being submitted
+  event.preventDefault();
+
+  // Get the number of days
+  const days = document.getElementById("textField").value;
+
+  // Create a WebSocket connection to the server
+  const ws = new WebSocket("ws://localhost:8000");
+
+  ws.onopen = function() {
+    // Send the request to the server
+    ws.send(days);
+  };
+
+  // Display the response from the server
+  ws.onmessage = function(event) {
+    const currentContent = document.getElementById("subscribe").innerHTML;
+    document.getElementById("subscribe").innerHTML = currentContent + "<br>" + "Privat Bank Api Answer :" + event.data;
+  };
+});
+
+// Get the exchange button
+const exchangeButton = document.getElementById("exchangeButton");
